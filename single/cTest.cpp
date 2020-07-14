1a83c8fd2f958defb99004fb22eb1facc24282a4
jun.cai <jun.cai@ecarx.com.cn>
2020-07-14 14:22:01 +0800

    vector erase

--- single/cTest.cpp
+++ single/cTest.cpp
@@ -1,16 +1,26 @@
 #include "stdio.h"
 #include <iostream>
 #include <time.h>
+#include <vector>
 //#include <ros/ros.h>
 
 
 int main(){
-time_t timep;
-time(&timep);
-//std::cout<<&timep<<std::endl;
-printf("%ld",&timep);
-//std<<cout<<ros::Time::now()<<std::endl;
-
+// time_t timep;
+// time(&timep);
+// //std::cout<<&timep<<std::endl;
+// printf("%ld",&timep);
+// //std<<cout<<ros::Time::now()<<std::endl;
+std::vector<int> vc;
+for (size_t i = 1; i < 6; i++)
+{
+    vc.push_back(i);
+}
+//vc.erase(vc.begin());
+vc.erase(vc.begin(), vc.begin()+1+1);
+for (size_t i = 0; i < vc.size(); i++)
+{
+    std::cout << vc[i] << std::endl;
+}
 return 1;
-
 }
